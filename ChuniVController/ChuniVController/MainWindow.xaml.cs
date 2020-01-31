1f2550f1b62a7ff199e24b9c342eb6f8a63b19ca
Nato Morichika <nat@nat.moe>
2020-01-31 05:59:25 -0500

    chunivc: init

---
+++ ChuniVController/ChuniVController/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChuniVController
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // make the window unforcusable
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+        private const int GWL_EXSTYLE = -20;
+
+        [DllImport("user32", SetLastError = true)]
+        private extern static int GetWindowLong(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32", SetLastError = true)]
+        private extern static int SetWindowLong(IntPtr hwnd, int nIndex, int dwNewValue);
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            WindowInteropHelper wih = new WindowInteropHelper(this);
+            int exstyle = GetWindowLong(wih.Handle, GWL_EXSTYLE);
+            exstyle |= WS_EX_NOACTIVATE;
+            SetWindowLong(wih.Handle, GWL_EXSTYLE, exstyle);
+        }
+    }
+}
---
+++ ChuniVController/ChuniVController/TouchPad.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChuniVController
+{
+    public partial class TouchPad : UserControl
+    {
+
+        // use old "keybd_event" so we can controll keyup/keydown ourself
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte key, byte scan, int flags, int info);
+
+        private const int KEYEVENTF_EXTENDEDKEY = 0x0001; // keydown
+        private const int KEYEVENTF_KEYUP = 0x0002; // keyup
+
+        public string idleColor { get; set; }
+        public string activeColor { get; set; }
+        public byte keyCode { get; set; }
+        public TouchPad()
+        {
+            InitializeComponent();
+        }
+
+        protected void Press()
+        {
+            keybd_event(keyCode, 0, KEYEVENTF_EXTENDEDKEY, 0);
+            LedStrip.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom(activeColor));
+        }
+
+        protected void Release()
+        {
+            keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
+            LedStrip.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom(idleColor));
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            Press();
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            Release();
+            base.OnMouseLeave(e);
+        }
+
+        // TouchEnter: key pressed
+        protected override void OnTouchEnter(TouchEventArgs e)
+        {
+            Press();
+            base.OnTouchEnter(e);
+        }
+
+        // TouchLeave: released
+        protected override void OnTouchLeave(TouchEventArgs e)
+        {
+            Release();
+            base.OnTouchLeave(e);
+        }
+    }
+}
