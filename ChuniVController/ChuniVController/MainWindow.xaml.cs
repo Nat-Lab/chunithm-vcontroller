c7a20308016773d7c9feaa62467a42c4d5d8fba5
Nato Morichika <nat@nat.moe>
2020-01-31 06:59:36 -0500

    chuni-vc: renderer logic

--- ChuniVController/ChuniVController/MainWindow.xaml.cs
+++ ChuniVController/ChuniVController/MainWindow.xaml.cs
@@ -24,9 +24,11 @@ namespace ChuniVController
             InitializeComponent();
         }
 
-        // make the window unforcusable
+        
         private const int WS_EX_NOACTIVATE = 0x08000000;
         private const int GWL_EXSTYLE = -20;
+
+        private TouchPad[] touchpads;
 
         [DllImport("user32", SetLastError = true)]
         private extern static int GetWindowLong(IntPtr hwnd, int nIndex);
@@ -36,10 +38,48 @@ namespace ChuniVController
 
         private void OnLoad(object sender, RoutedEventArgs e)
         {
-            WindowInteropHelper wih = new WindowInteropHelper(this);
+            // make the window unforcusable
+            /*WindowInteropHelper wih = new WindowInteropHelper(this);
             int exstyle = GetWindowLong(wih.Handle, GWL_EXSTYLE);
             exstyle |= WS_EX_NOACTIVATE;
-            SetWindowLong(wih.Handle, GWL_EXSTYLE, exstyle);
+            SetWindowLong(wih.Handle, GWL_EXSTYLE, exstyle);*/
+
+            touchpads = new TouchPad[] { Key1, Key2, Key3, Key4, Key5, Key6, Key7, Key8, Key9, Key10, Key11, Key12, Key13, Key14, Key15, Key16 };
+            Render();
+        }
+
+        private void Render()
+        {
+            double keyw = 130;
+            double keyh = 250;
+            double airh = 300;
+            double.TryParse(KeyWidth.Text, out keyw);
+            double.TryParse(KeyHeight.Text, out keyh);
+            double.TryParse(AirHeight.Text, out airh);
+
+            double toth = keyh + airh;
+            double totw = keyw * 16 + 20;
+
+            this.Height = toth + 75;
+            this.Width = totw;
+
+            Air.Height = airh;
+            Air.Width = totw;
+
+            double left_offset = 0;
+
+            foreach (TouchPad t in touchpads)
+            {
+                t.Height = keyh;
+                t.Width = keyw;
+                t.Margin = new Thickness(left_offset, 0, 0, 35);
+                left_offset += keyw;
+            }
+        }
+
+        private void DoApply(object sender, RoutedEventArgs e)
+        {
+            Render();
         }
     }
 }
